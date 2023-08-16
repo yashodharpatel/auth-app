09d292f4c5b35fa40dc0a4a2b7695f8c8a56a8c9
Yashodhar Patel <patelyashodhar012@gmail.com>
2023-08-16 20:46:39 +0530

    signup completed

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,58 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import {NextRequest, NextResponse} from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const {username, email, password} = reqBody;
+
+        console.log(reqBody);
+
+        // validate the email
+        const validateEmail = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+        if(!validateEmail.test(email)) {
+            return NextResponse.json(
+                {message: "Please enter a valid email"},
+                {status: 400}
+            )
+        }
+
+        // check if the user already exists
+        const user = await User.findOne({email});
+        if(user) {
+            return NextResponse.json(
+                {message: "User already exists"},
+                {status: 400}
+            )
+        }
+
+        // hash the password
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        // create new user
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        const savedUser = await newUser.save();
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser
+        });
+    } catch (error: any) {
+        return NextResponse.json(
+            {message: error.message},
+            {status: 500}
+        )
+    }
+}
+
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,26 +1,46 @@
 "use client";
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/link';
 import {useRouter} from "next/navigation";
 import axios from "axios";
+import {toast} from "react-hot-toast";
 
 const Signup = () => {
+    const router = useRouter();
     const [user, setUser] = useState({
-        email: "",
         username: "",
+        email: "",
         password: ""
     })
     const [buttonDisabled, setButtonDisabled] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
     const onSignup = async () => {
-
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            toast.success(response?.data?.message);
+            console.log(response.data);
+            router.push("/login");
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message);
+            console.log(error?.response);
+        } finally {
+            setLoading(false);
+        }
     }
+
+    useEffect(() => {
+        if (user.username.length > 0 && user.email.length > 0 && user.password.length > 0)
+            setButtonDisabled(false);
+        else
+            setButtonDisabled(true);
+    }, [user])
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>{loading ? "Processing" : "Signup"}</h1>
-            <hr />
+            <hr/>
             <label htmlFor="username">username</label>
             <input
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
