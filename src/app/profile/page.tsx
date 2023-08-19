39e13f4728e4e5b34e03d7f29cf2967bbc932614
Yashodhar Patel <patelyashodhar012@gmail.com>
2023-08-20 00:07:59 +0530

    profile added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,10 +3,12 @@ import axios from "axios";
 import Link from "next/link";
 import {useRouter} from "next/navigation";
 import {toast} from "react-hot-toast";
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Profile = () => {
     const router = useRouter();
+    const [user, setUser] = useState({});
+    const [id, setId] = useState("");
     const logout = async () => {
         try {
             const response = await axios.get("/api/users/logout");
@@ -18,10 +20,23 @@ const Profile = () => {
             console.log(error?.response);
         }
     }
+
+    const getUserDetails = async () => {
+        const response = await axios.get('/api/users/userinfo');
+        console.log(response?.data);
+        setUser(response?.data);
+        setId(response?.data?._id);
+    }
+
+    useEffect(() => {
+        getUserDetails();
+    }, []);
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-4xl">Profile</h1>
             <p>Profile page</p>
+            <h2>{user && <Link href={`/profile/${id}`} >My Profile</Link>}</h2>
             <button onClick={logout} className="bg-blue-500 text-white py-2 px-4 rounded mt-2">Logout</button>
         </div>
     )
