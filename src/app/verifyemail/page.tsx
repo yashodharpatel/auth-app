1b4b908d62ba3fd3f04b0a8deb4cb84cc7764430
Yashodhar Patel <patelyashodhar012@gmail.com>
2023-09-14 22:26:05 +0530

    updated

---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, {useEffect, useState} from 'react';
+import Link from 'next/link';
+import {useRouter} from "next/navigation";
+import axios from "axios";
+import {toast} from "react-hot-toast";
+
+const VerifyEmail = () => {
+    return (
+        <div>VerifyEmail</div>
+    )
+}
+export default VerifyEmail;
--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,5 +1,5 @@
 import {NextRequest} from "next/server";
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 export const getDataFromToken = (request: NextRequest) => {
     try{
